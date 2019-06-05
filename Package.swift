5fcec8bf816890948d3390d158b7c8d4260a3a70
Rob Amos <bok@unsignedapps.com>
2019-06-06 07:02:23 +1000

    Updated to Swift Package Manager 5.0 structure.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
-import PackageDescription
+// swift-tools-version:5.0
 
+import PackageDescription
 
 let package = Package(
   name: "JSONSchema",
-  exclude: [
-    "Tests/Cases",
-  ]
+	targets: [
+		.target(name: "JSONSchema", dependencies: []),
+		.testTarget(name: "JSONSchemaTests", dependencies: [ "JSONSchema" ])
+	]
 )
